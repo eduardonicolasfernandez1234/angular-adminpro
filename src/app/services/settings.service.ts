1a9bcfdfbc463e9b5cca3aa64a9d62c6863b18c6
Eduardo Nicolas Fernandez <eduardonicolasfernandez1234@gmail.com>
2022-04-02 23:51:59 -0400

    Routing links, change themes

---
+++ src/app/services/settings.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+
+  public linkTheme = document.querySelector('#theme');
+
+  constructor() {
+    const url = localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
+    this.linkTheme?.setAttribute('href', url);
+  }
+
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${ theme }.css`;
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    const links = document.querySelectorAll('.selector');
+    links.forEach((element) => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnFindUrl = `./assets/css/colors/${ btnTheme }.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+      if (btnFindUrl === currentTheme) {
+        element.classList.add('working');
+      }
+    });
+  }
+}
